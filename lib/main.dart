76497b0938144c98dd33e27f3561c1fa4bbc6808
Sadiq Goni <sadiqgoni13@gmail.com>
2023-08-02 11:21:22 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertask/screens/home_page.dart';
+import 'package:fluttertask/index.dart';
 import 'package:sizer/sizer.dart';
-import 'builder/category_provider.dart';
-import 'builder/lesson_provider.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(Sizer(builder: (context, orientation, deviceType) {
-    return   MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ProgramProvider()),
-        ChangeNotifierProvider(create: (_) => LessonProvider()),
-      ],
-      child: MyApp(),
-    );
+    return MyApp();
   }));
 }
-
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
     );
   }
 }
-
-
